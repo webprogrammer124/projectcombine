ac32f885431ef15ae48f1dcf4e9f7c708f3186fe
Your Name <you@example.com>
2021-01-11 12:55:43 +0500

    done

---
+++ backhend/Users.js
@@ -0,0 +1,5 @@
+let users = [
+    {name:'seema',id:1,email:'seemagreat.911@gmail.com',password:'1111'},
+    {name:'bisma',id:2,email:'bisma@gmail.com',password:'2222'}    
+]
+module.exports = users
